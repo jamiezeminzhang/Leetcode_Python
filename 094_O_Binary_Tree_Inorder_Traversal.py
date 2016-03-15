f5fdfb0936444ba6c55bfd7d90a32e94c2b29632
jamiezeminzhang <jamie.zeminzhang@gmail.com>
2016-03-15 19:33:47 -0400

    ~

--- 094_O_Binary_Tree_Inorder_Traversal.py
+++ 094_O_Binary_Tree_Inorder_Traversal.py
@@ -45,9 +45,9 @@ Subscribe to see which companies asked this question
 
 　　　　　　　　　　　　　　 2　　  3
 
-　　　　　　　　　　　　　  /   \　 /   \
+　　　　　　　　　　　　  /   \　 /   \
 
-   　　　　　　　　　　　　4     5  6    7
+    　　　　　　　　　　4     5  6    7
 
 我们使用一个栈来解决问题。步骤如下：
 
--- 095_O_Unique_Binary_Search_Trees_II.py
+++ 095_O_Unique_Binary_Search_Trees_II.py
@@ -32,6 +32,25 @@ Here's an example:
      5
 The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
 
+*** Beautiful Recursive Solution ***
+class Solution(object):
+    def generateTrees(self, n):
+        if n == 0:
+            return [[]]
+        return self.dfs(1, n+1)
+
+    def dfs(self, start, end):
+        if start == end:
+            return None
+        result = []
+        for i in xrange(start, end):
+            for l in self.dfs(start, i) or [None]:
+                for r in self.dfs(i+1, end) or [None]:
+                    node = TreeNode(i)
+                    node.left, node.right  = l, r
+                    result.append(node)
+        return result
+
 @author: zeminzhang
 """
 
