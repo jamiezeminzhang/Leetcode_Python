546726d028a1ac0fd786e1366f56964bedabac0c
jamiezeminzhang <jamie.zeminzhang@gmail.com>
2016-03-14 23:27:14 -0400

    Revert "Revert "Questions,s""
    This reverts commit f64394a80ac81b8272cc14c85d131d47ef7eebff.

--- 026_remove_duplicates_from_sorted_array.py
+++ 026_remove_duplicates_from_sorted_array.py
@@ -16,21 +16,6 @@ Given input array nums = [1,1,2],
 Your function should return length = 2, with the first two elements of nums
  being 1 and 2 respectively. It doesn't matter what you leave beyond the new length. 
 
-class Solution:
-    # @param a list of integers
-    # @return an integer
-    def removeDuplicates(self, A):
-        if len(A) == 0:
-            return 0
-        j = 0
-        for i in range(0, len(A)):
-            if A[i] != A[j]:
-                A[i], A[j+1] = A[j+1], A[i]
-                j = j + 1
-        return j+1
-
-		
-
 @author: zzhang
 """
 
@@ -40,16 +25,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) <=1: return len(nums)
-        
-        left = 0
-        for i in range(1,len(nums)):
-            if nums[left] == nums[i]:
-                continue
-            else:
-                left += 1
-                nums[left] = nums[i]
-        return left+1
-
+        i = 0
+        for num in nums:
+            if i==0 or num > nums[i-1]:
+                nums[i] = num
+                i+=1
+        return i
 sol = Solution()
 print sol.removeDuplicates([1,1,2,3,4,4,5])
