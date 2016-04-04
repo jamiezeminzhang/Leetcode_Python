ed496d1ed40ba16f6fcf30dba86eeb478e066d1f
jamiezeminzhang <jamie.zeminzhang@gmail.com>
2016-04-04 07:17:21 -0400

    Solution Updated

--- 129_Sum_Root_To_Leaf_Numbers.py
+++ 129_Sum_Root_To_Leaf_Numbers.py
@@ -2,6 +2,26 @@
 """
 Created on Fri Feb  5 01:40:41 2016
 
+129. Sum Root to Leaf Numbers
+
+Total Accepted: 73158 Total Submissions: 225538 Difficulty: Medium
+
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25.
+
 @author: zeminzhang
 """
 
--- 130_OO_Surrounded_Regions.py
+++ 130_OO_Surrounded_Regions.py
@@ -96,34 +96,6 @@ class Solution:
                 if board[i][j] == 'D': board[i][j] = 'O'
                 elif board[i][j] == 'O': board[i][j] = 'X'
 
-class my_Solution(object):
-    def solve(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: void Do not return anything, modify board in-place instead.
-        """
-        def fill(x, y):
-            if x<0 or x>m-1 or y<0 or y > n-1 or board[x][y] != 'O': return
-            queue.append((x,y))
-            board[x][y] = 'D'
-        
-        def bfs(x,y):
-            if board[x][y] == 'O': fill(x,y)
-            while queue:
-                curr = queue.pop(0); curr1,curr2 = curr[0],curr[1]
-                fill(curr1+1,curr2);fill(curr1-1,curr2);fill(curr1,curr2+1);fill(curr1,curr2-1)
-        
-        if len(board) ==0: return 
-        m,n = len(board), len(board[0]);queue = []
-        for i in range(m):
-            bfs(i,0);bfs(i,n-1)
-        for j in range(n):
-            bfs(0,j);bfs(m-1,j)
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == 'D': board[i][j] = 'O'
-                elif board[i][j] == 'O': board[i][j] = 'X'
-
 sol = Solution()
 #board = [ ['X','X','X'] ,['X','0','X'],['X','X','X']]
 board = [ ['O'] ]
