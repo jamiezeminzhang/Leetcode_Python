a32aa71a0d6e746ee8d75d3a44dd5625733eebac
jamiezeminzhang <zeminzhang@Zemins-MacBook-Pro.local>
2016-07-07 09:04:50 -0700

    Solution Updated

---
+++ hash/368_OO_Largest_divisible_subset.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Friday July 7 09:02:29 2016
+
+368. Largest Divisible Subset
+
+Total Accepted: 2922
+Total Submissions: 9708
+Difficulty: Medium
+
+Given a set of distinct positive integers, find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies: Si % Sj = 0 or Sj % Si = 0.
+
+If there are multiple solutions, return any subset is fine.
+
+Example 1:
+
+nums: [1,2,3]
+
+Result: [1,2] (of course, [1,3] will also be ok)
+Example 2:
+
+nums: [1,2,4,8]
+
+Result: [1,2,4,8]
+
+** Pochmann’s answer is as crazy as usual.
+
+@author: zzhang
+"""
+
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        s = {-1: set()}
+        for x in sorted(nums):
+            s[x] = max((s[d] for d in s if x%d==0), key=len) | {x}
+        return list(max(s.values(), key=len))
---
+++ sorting_and_search/367_Valid_Perfect_Square.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Friday July 5 08:58:24 2016
+
+367. Valid Perfect Square
+
+Total Accepted: 5007
+Total Submissions: 13844
+Difficulty: Medium
+
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Returns: True
+Example 2:
+
+Input: 14
+Returns: False
+
+@author: zzhang
+"""
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num==0 or num==1: return True
+        left, right = 0, num/2
+        while left<=right:
+            mid = (left+right)/2
+            if mid**2==num: return True
+            elif mid**2<num:
+                left = mid+1
+            else:
+                right = mid-1
+        return False
