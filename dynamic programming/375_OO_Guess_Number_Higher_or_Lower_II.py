151d7915f6370e1c7a27cf6e86dc5c04469320ab
jamiezeminzhang <zeminzhang@Zemins-MacBook-Pro.local>
2016-07-27 09:27:51 -0700

    Solution Updated

---
+++ dynamic programming/375_OO_Guess_Number_Higher_or_Lower_II.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday July 27 09:19:23 2016
+
+375. Guess Number Higher or Lower II 
+
+Total Accepted: 2899
+Total Submissions: 10081
+Difficulty: Medium
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number I picked is higher or lower.
+
+However, when you guess a particular number x, and you guess wrong, you pay $x. You win the game when you guess the number I picked.
+
+Example:
+
+n = 10, I pick 8.
+
+First round:  You guess 5, I tell you that it's higher. You pay $5.
+Second round: You guess 7, I tell you that it's higher. You pay $7.
+Third round:  You guess 9, I tell you that it's lower. You pay $9.
+
+Game over. 8 is the number I picked.
+
+You end up paying $5 + $7 + $9 = $21.
+Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
+
+Hint:
+
+The best strategy to play the game is to minimize the maximum loss you could possibly face. Another strategy is to minimize the expected loss. Here, we are interested in the first scenario.
+Take a small example (n = 3). What do you end up paying in the worst case?
+Check out this article if you're still stuck.
+The purely recursive implementation of minimax would be worthless for even a small n. You MUST use dynamic programming.
+As a follow-up, how would you modify your code to solve the problem of minimizing the expected loss, instead of the worst-case loss?
+
+@author: zzhang
+
+*** Please understand why we only take half in dp ***
+
+"""
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def getMoneyAmount(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        need = [[0]*(n+1) for _ in range(n+1)]
+        for lo in range(n,0,-1):
+            for hi in range(lo+1,n+1):
+                need[lo][hi] = min(x + max(need[lo][x-1], need[x+1][hi]) \
+                for x in range(lo,hi))
+        return need[1][n]
