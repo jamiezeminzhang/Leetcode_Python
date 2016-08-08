28573c8eb4614dd02d31e5e170fa730a63b16579
jamiezeminzhang <zeminzhang@Zemins-MacBook-Pro.local>
2016-08-08 10:30:00 -0700

    Solution Updated

---
+++ ALL_SOLUTIONS/298_Binary_Tree_Longest_Consecutive_Sequence.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Aug 6 2016 9:49 AM
+
+298. Binary Tree Longest Consecutive Sequence
+
+Total Accepted: 11465
+Total Submissions: 30345
+Difficulty: Medium
+
+Given a binary tree, find the length of the longest consecutive sequence path.
+
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+
+For example,
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+Longest consecutive sequence path is 3-4-5, so return 3.
+   2
+    \
+     3
+    / 
+   2    
+  / 
+ 1
+Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+
+@author: zzhang
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def longestConsecutive(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        self.max_length = 0
+        def dfs(node, cur_length):
+            if cur_length > self.max_length: 
+                self.max_length = cur_length
+            if node.left:
+                if node.left.val - node.val == 1:
+                    dfs(node.left, cur_length+1)
+                else:
+                    dfs(node.left, 1)
+            if node.right:
+                if node.right.val - node.val == 1:
+                    dfs(node.right, cur_length+1)
+                else:
+                    dfs(node.right, 1)
+        dfs(root, 1)
+        return self.max_length
---
+++ _Google/056_OO_Merge_Intervals.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 21:33:28 2016
+
+56. Merge Intervals
+
+Total Accepted: 69326 Total Submissions: 269203 Difficulty: Hard
+
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+
+解题思路：先将区间按照每个start的值来排序，排好序以后判断一个区间的start值是否处在前
+一个区间中，如果在前一个区间中，那么合并；如果不在，就将新区间添加。
+
+@author: zeminzhang
+"""
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        length = len(intervals)
+        if length == 0: return []
+        if length == 1: return intervals
+            
+        intervals.sort(key = lambda x:x.start)
+        
+        res = [intervals[0]]
+        for i in range(1,length):
+            new = intervals[i]
+            old = res.pop()
+            if new.start >= old.start and new.start <= old.end:
+                if new.end > old.end :
+                    res.append(Interval(old.start, new.end))
+                else:
+                    res.append(old)
+            if new.start > old.end:
+                res.append(old)
+                res.append(new)
+        
+        return res
+                
+sol = Solution()
+a = Interval(1,4)
+b = Interval(5,6)
+c = Interval(3,3)
+d = Interval(1,3)
+c = sol.merge([a,b])
+for i in c:
+    print i.start, i.end
+                    
+        
+
+                
+            
