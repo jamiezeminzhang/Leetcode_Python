cfc3b5ec485e228300a90393fc7241f73243be39
jamiezeminzhang <zeminzhang@Zemins-MacBook-Pro.local>
2016-08-08 17:38:40 -0700

    Solution Updated
    Solution Updated

--- ALL_SOLUTIONS/155_OO_Min_Stack.py
+++ ALL_SOLUTIONS/155_OO_Min_Stack.py
@@ -12,35 +12,30 @@ pop() -- Removes the element on top of the stack.
 top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
 
-用一个minstack来存储最小值。很巧妙。
 
 @author: zeminzhang
 """
 
 class MinStack(object):
-
     def __init__(self):
         """
         initialize your data structure here.
         """
         self.stack = []
-        self.minstack = []
 
     def push(self, x):
         """
         :type x: int
-        :rtype: void
+        :rtype: nothing
         """
-        self.stack.append(x)
-        if not self.minstack or self.minstack[-1]>=x:
-            self.minstack.append(x)
+        curMin = self.getMin()
+        if curMin==None or x<curMin: curMin = x
+        self.stack.append((x,curMin))
 
     def pop(self):
         """
-        :rtype: void
+        :rtype: nothing
         """
-        if self.stack[-1] == self.minstack[-1]:
-            self.minstack.pop()
         self.stack.pop()
         
 
@@ -48,19 +43,12 @@ class MinStack(object):
         """
         :rtype: int
         """
-        return self.stack[-1]
+        if self.stack: return self.stack[-1][0]
+        else: return None
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.minstack[-1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
-        
+        if self.stack: return self.stack[-1][1]
+        else: return None        
---
+++ ALL_SOLUTIONS/340_OO_Longest_Substring_With_At_Most_K_Distinct_Characters.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+2016/08/08 17:37 PST
+
+340. Longest Substring with At Most K Distinct Characters 
+
+Total Accepted: 4716
+Total Submissions: 12156
+Difficulty: Hard
+
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+For example, Given s = “eceba” and k = 2,
+
+T is "ece" which its length is 3.
+
+@author: zzhang
+"""
+
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        res, j, ctr = min(k, len(s)), -1, collections.defaultdict(int)
+        for i, c in enumerate(list(s)):
+            ctr[c] += 1
+            while len(ctr)>k:
+                j+=1
+                ctr[s[j]]-=1
+                if ctr[s[j]]==0:
+                    del ctr[s[j]]
+            res = max(res, i-j)
+        return res
