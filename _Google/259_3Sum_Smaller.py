7c287d20d3875f9357626595e55f9f8f216772ae
jamiezeminzhang <zeminzhang@Zemins-MacBook-Pro.local>
2016-08-23 15:25:53 -0700

    Solution Updated

---
+++ _Google/259_3Sum_Smaller.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday Aug 23 09:11:20 2016
+
+259. 3Sum Smaller
+
+Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
+
+For example, given nums = [-2, 0, 1, 3], and target = 2.
+
+Return 2. Because there are two triplets which sums are less than 2:
+
+[-2, 0, 1]
+[-2, 0, 3]
+Follow up:
+Could you solve it in O(n2) runtime?
+
+Hide Company Tags Google
+Hide Tags Array Two Pointers
+Hide Similar Problems (M) 3Sum (M) 3Sum Closest
+
+        
+@author: Jamie
+"""
+
+class Solution(object):
+    def threeSumSmaller(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        ans = 0
+        while len(nums)>=3:
+            cur_num = nums.pop()
+            ans += self.twoSum(nums, target-cur_num)
+        return ans
+    
+    def twoSum(self, nums, target):
+        res = 0
+        p1, p2 = 0, len(nums)-1
+        while p1<p2:
+            cur = nums[p1]+nums[p2]
+            if cur<target:
+                res += (p2-p1)
+                p1 += 1
+            else:
+                p2-=1
+        return res
+                
+                
---
+++ array/280_OO_Wiggle_Sort.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+2016/08/11 08:56 PST
+
+280. Wiggle Sort  
+
+Total Accepted: 14223
+Total Submissions: 27501
+Difficulty: Medium
+
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+
+＊＊奇数index应该大，偶数index应该小
+
+class Solution(object):
+    def wiggleSort(self, nums):
+        for i in range(1, len(nums)):
+            if (i%2==1 and nums[i]<nums[i-1]) or (i%2==0 and nums[i]>nums[i-1]):
+                nums[i-1], nums[i] = nums[i], nums[i-1]
+
+"""
+
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)<=1: return
+        
+        index = 1
+        while index<len(nums):
+            if index < len(nums)-1:
+                if nums[index] < max(nums[index-1], nums[index+1]):
+                    if nums[index-1]>nums[index+1]:
+                        nums[index-1], nums[index] = nums[index], nums[index-1]
+                    else:
+                        nums[index+1], nums[index] = nums[index], nums[index+1]
+            else:
+                if nums[index]<nums[index-1]:
+                    nums[index-1], nums[index] = nums[index], nums[index-1]
+            index += 2
